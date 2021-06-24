326e2321225833d8d110172039ad5515770e7987
Christopher Owen <57742240+ChristopherOwen1212@users.noreply.github.com>
2021-06-24 19:47:22 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('Simple rest api');
+});
+
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
+});
+
+var Products = require("./product.json");
+
+//GET all list
+app.get("/product/listall", (req, res) => {
+    res.json(Products);
+});
+
+//GET detail by id
+app.get("/product/:id", (req, res) => {
+    res.json(Products.find((product) => {
+        return req.params.id == product.id
+    }))  
+});
